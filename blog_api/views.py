4fb2e97eb005e0313e95c6a0389cb1d33b121aae
Miguel Soto <msoto056@gmail.com>
2021-10-12 18:15:53 -0600

    Update Models to remove error

--- blog_api/views.py
+++ blog_api/views.py
@@ -10,7 +10,7 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
