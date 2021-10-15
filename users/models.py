c61e7e27bf62cd8c20c1f41b3ca1f21ad1d6a89a
Miguel Soto <msoto056@gmail.com>
2021-10-14 20:50:41 -0600

    Changes to Serializers

--- users/models.py
+++ users/models.py
@@ -38,7 +38,8 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(_('email address'), unique=True)
     user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
     start_date = models.DateTimeField(default=timezone.now)
     about = models.TextField(_(
         'about'), max_length=500, blank=True)
@@ -48,7 +49,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name', 'first_name']
+    REQUIRED_FIELDS = ['user_name', 'first_name', 'last_name']
 
     def __str__(self):
         return self.user_name
